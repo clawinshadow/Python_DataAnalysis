3e3c02760a9f8f8ba5cff8128a909c086855b1ee
Huang Fan <hfyier@sina.com>
2017-05-15 23:17:54 +0800

    Add files via upload

--- MachineLearning/Scikit_Learn/feature_selection.py
+++ MachineLearning/Scikit_Learn/feature_selection.py
@@ -122,4 +122,16 @@ MI = sfs.mutual_info_classif(X, y, discrete_features=True)
 print('Mutual Information by sklearn: ', MI)
 print('MI = I: ', np.allclose(I, MI))
 
-
+# f_regression, 将每一列与目标向量y都做一次一元线性回归，分别计算各自的F和P值
+# 其原理可以参考之前写的linear_regression.py
+# 先计算SST, SSR, SSE 然后 F = (SSR / 1) / ( SSE / (n - 2) ) ~ F(1, n-2)
+print('{0:-^70}'.format('f regession'))
+y = np.array([0.9,1.1,4.8,3.2,7.8,2.7,1.6,12.5,1.0,2.6,0.3,4.0,0.8,3.5,10.2,3.0,\
+              0.2,0.4,1.0,6.8,11.6,1.6,1.2,7.2,3.2])
+x = np.array([67.3,111.3,173,80.8,199.7,16.2,107.4,185.4,96.1,72.8,64.2,132.2,58.6,\
+              174.6,263.5,79.3,14.8,73.5,24.7,139.4,368.2,95.7,109.6,196.2,102.2])
+X = x[:, np.newaxis]
+F, pval = sfs.f_regression(X, y.T)
+print('X: ', X)
+print('y: ', y)
+print('f_regression of X & y: ', F)
