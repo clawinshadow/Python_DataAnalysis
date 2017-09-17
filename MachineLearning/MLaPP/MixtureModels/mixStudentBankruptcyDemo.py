f0d81b9955565c31d4bf8a787053eb1fa221e052
Huang Fan <hfyier@sina.com>
2017-09-18 00:46:36 +0800

    Add files via upload

--- MachineLearning/MLaPP/MixtureModels/mixStudentBankruptcyDemo.py
+++ MachineLearning/MLaPP/MixtureModels/mixStudentBankruptcyDemo.py
@@ -6,6 +6,8 @@ import scipy.linalg as sl
 import sklearn.preprocessing as sp
 import matplotlib.pyplot as plt
 
+np.random.seed(0)
+
 def pdf(x, mu, sigma, dof):
     '''
     多元T分布的概率密度函数
@@ -21,7 +23,14 @@ def pdf(x, mu, sigma, dof):
 
     return (val_1 / val_2) * val_3**(-(dof + d) / 2) 
 
-def GetR(
+def GetR(x, pi, mu, sigma, dof):
+    r = []
+    total = 0
+    for i in range(len(pi)):
+        prob = pi[i] * pdf(x, mu[i], sigma[i], dof[i])
+        total += prob
+        r.append(prob)
+    return r / total
 
 # load data
 data = sio.loadmat("bankruptcy.mat")
@@ -30,3 +39,7 @@ x_train = sp.StandardScaler().fit_transform(x)  # 标准化
 y_train = data['data'][:, 0]
 
 # Fit Mix-Student model
+K = 2
+mu_init = np.r_[np.random.randn(1, 2), np.random.randn(1, 2)]
+sigma_init = np.array([np.eye(2), np.eye(2)])
+dof_init = np.array([10, 10])
