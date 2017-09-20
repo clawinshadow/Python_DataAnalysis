a04775c2ad9c0aeb88c5759856fdf0c2a7d1ca17
Huang Fan <hfyier@sina.com>
2017-09-20 08:03:14 +0800

    Add files via upload

--- MachineLearning/MLaPP/MixtureModels/mixStudentBankruptcyDemo.py
+++ MachineLearning/MLaPP/MixtureModels/mixStudentBankruptcyDemo.py
@@ -3,9 +3,11 @@ import numpy as np
 import scipy.io as sio
 import scipy.stats as ss
 import scipy.linalg as sl
+import scipy.optimize as so
 import sklearn.preprocessing as sp
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import mahalanobis
+from scipy.special import digamma, gammaln
 from mixGaussFit import *
 
 '''
@@ -90,11 +92,12 @@ def EStep(X, pi, mu, sigma, dof):
     return np.array(rMat), np.array(zMat)  # N * K
 
 # M step 中就不再需要pi, mu ... 这些参数了，已经拿到期望值r和z了
-def MStep(X, r, z):
+def MStep(X, r, z, dof, updateDof=False):
     K, N = r.shape[1], X.shape[0]
     mu = []
     sigma = []
     pi = []
+    dof_new = []
     for i in range(K):
         rk = r[:, i]
         zk = z[:, i]
@@ -107,8 +110,43 @@ def MStep(X, r, z):
         sigma.append(sigma_k)
         pi.append(pi_k)
 
-    return np.array(mu), np.array(sigma), np.array(pi)
-        
+    # update dof
+    if updateDof:
+        L = np.zeros((N, K))
+        for i in range(K):
+            sigma_k = sigma[i]
+            logdet = 0.5 * math.log(sl.det(sigma_k))
+            logmix = math.log(pi[i])
+            distances = []
+            inv_sigma_k = sl.inv(sigma_k)
+            for j in range(len(X)):
+                xj = X[j]
+                distances.append(mahalanobis(xj.ravel(), mu[i].ravel(), inv_sigma_k))
+            distances = np.array(distances).ravel()
+            L[:, i] = GetLk(dof[i], logmix, logdet, distances, X.shape[1])
+
+        print('L: \n', L)
+
+        for i in range(K):
+            sigma_k = sigma[i]
+            logdet = 0.5 * math.log(sl.det(sigma_k))
+            logmix = math.log(pi[i])
+            distances = []
+            inv_sigma_k = sl.inv(sigma_k)
+            for j in range(len(X)):
+                xj = X[j]
+                distances.append(mahalanobis(xj.ravel(), mu[i].ravel(), inv_sigma_k))
+            distances = np.array(distances).ravel()
+            
+            bnds = ((0, None),)
+            x_init = (dof[i],)
+            res = so.minimize(dofFunc, x0=x_init, args=(logmix, logdet, distances, X.shape[1], L, i), bounds=bnds)
+            dof_new.append(res.x)
+    else:
+        dof_new = dof
+
+    return np.array(mu), np.array(sigma), np.array(pi), np.array(dof_new)
+
 def UpdateSigma(X, mu, r, z, k):
     rk = np.sum(r[:, k])
     val_1 = np.zeros((X.shape[1], X.shape[1]))
@@ -122,32 +160,56 @@ def UpdateSigma(X, mu, r, z, k):
     
     return (1/rk) * (val_1 - val_2)
 
-def mixStudentFit(X, K=2, maxIter=100):
+def GetLk(x, logmix, logdets, distances, D):
+    val1 = logmix + gammaln((x+D)/2) - gammaln(x/2) - logdets - D * math.log(x) / 2
+    val2 = 0.5 * (x + D) * np.log1p(distances/x)
+    gap = val1 - val2
+    return gap
+
+def dofFunc(x, logmix, logdets, distances, D, L, k):
+    val1 = logmix + gammaln((x+D)/2) - gammaln(x/2) - logdets - D * math.log(x) / 2
+    val2 = 0.5 * (x + D) * np.log1p(distances/x)
+    gap = val1 - val2
+    L[:, k] = gap
+    lse = np.log(np.sum(np.exp(L), axis=1))
+    return -np.sum(lse)
+
+    # return N * gammaln(x/2) - (N*x/2) * math.log(x/2) - (x/2) * val
+    # return N * digamma(x/2) - N * math.log(x/2) - N - val
+
+def mixStudentFit(X, K=2, maxIter=50):
     mu = np.r_[np.random.randn(1, 2), np.random.randn(1, 2)]
-    sigma = np.array([np.eye(2), np.eye(2)])
-    dof = np.tile(5, K)
-    pi = np.tile(1/K, K)
+    sigma = GetSigmaInit(X.shape[1], 2, K)
+    dof = np.tile(10, K)
+    pi = GetPiInit(2, K)
 
     for i in range(maxIter):
         r, z = EStep(X, pi, mu, sigma, dof)
-        mu_new, sigma_new, pi_new = MStep(X, r, z)
+        mu_new, sigma_new, pi_new, dof_new = MStep(X, r, z, dof, True)
         print('{0:-^60}'.format('Iteration ' + str(i)))
         print('mu: \n', mu_new)
         print('sigma: \n', sigma_new)
         print('pi: ', pi_new)
+        print('dof: ', dof_new)
         pi = pi_new
         mu = mu_new
         sigma = sigma_new
+        dof = dof_new
 
     return pi, mu, sigma, dof
 
-def mixGaussFit(X, K=2, maxIter=100):
+def mixGaussFit(X, K=2, maxIter=50):
     mu = np.r_[np.random.randn(1, 2), np.random.randn(1, 2)]
-    sigma = np.array([np.eye(2), np.eye(2)])
-    pi = np.tile(1/K, K)
+    sigma = GetSigmaInit(X.shape[1], 2, K)
+    pi = GetPiInit(2, K)
+    prior_mu = np.zeros(X.shape[1])
+    prior_k = 0.01
+    prior_dof = X.shape[1] + 1
+    prior_s = 0.1 * np.eye(X.shape[1])
+    prior = [prior_mu, prior_k, prior_dof, prior_s]
 
     for i in range(maxIter):
-        r, pi_new, mu_new, sigma_new = EM(X, pi, mu, sigma)
+        r, pi_new, mu_new, sigma_new = EM_MAP(X, pi, mu, sigma, prior)
         print('{0:-^60}'.format('Iteration ' + str(i)))
         print('mu: \n', mu_new)
         print('sigma: \n', sigma_new)
@@ -163,7 +225,25 @@ data = sio.loadmat("bankruptcy.mat")
 x = data['data'][:, 1:]
 x_train = sp.StandardScaler().fit_transform(x)  # 标准化
 y_train = data['data'][:, 0]
+Bankrupt = x_train[y_train == 0]
+Solvent = x_train[y_train == 1]
+
+# plots
+fig = plt.figure(figsize=(11, 5))
+fig.canvas.set_window_title("mixStudentBankruptcyDemo")
+
+plt.subplot(121)
+plt.title('14 errors using gauss (red=error)')
+plt.axis([-5, 2, -7, 3])
+plt.xticks(np.linspace(-5, 2, 8))
+plt.yticks(np.linspace(-7, 3, 11))
+plt.plot(Bankrupt[:, 0], Bankrupt[:, 1], 'bo', ls='none', fillstyle='none', label='Bankrupt')
+plt.plot(Solvent[:, 0], Solvent[:, 1], 'b^', ls='none', fillstyle='none', label='Solvent')
+plt.legend()
 
 # Fit Mix-Student model
 res = mixStudentFit(x_train)
-res2 = mixGaussFit(x_train)
+# res2 = mixGaussFit(x_train)
+
+
+plt.show()
